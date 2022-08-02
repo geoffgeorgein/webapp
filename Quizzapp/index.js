f56ec524a2b484fd5fa60bc960ed7b56d9b86258
Geoff George <52931060+geoffgeorgein@users.noreply.github.com>
2022-08-02 22:23:47 +0530

    Add files via upload

---
+++ Quizzapp/index.js
@@ -0,0 +1,49 @@
+
+const quizData=[
+    {
+
+        q1:"Which is the captail of India",
+        a: 'Mumbai',
+        b:'Delhi',
+        c: 'Chennai',
+        d: 'Kolkata',
+        correct:'c'
+    },
+
+    {
+        q2:"Which is the most popular programming language",
+        a:'JavaScript',
+        b:'Java',
+        c:'C++',
+        d:'Python',
+        correct:'a'
+    },
+
+    {
+        q3:"Who was the founder of Apple Inc",
+        a:'Steve Wozniak',
+        b:'Bill Gates',
+        c:'Elon Musk',
+        d:'Steve Jobbs',
+        correct:'d'
+    },
+
+    {
+        q4:"What does the Internet prefix WWW stand for?",
+        a:'Western Washington World',
+        b:'Wide Width Wickets',
+        c:'World Wide Web',
+        d:'Worldwide Weather',
+        correct:'c'
+    },
+
+    {
+        q5:"Which of these products is not made by the Apple Corporation",
+        a:'Ipod',
+        b:'Ipad',
+        c:'IMax',
+        d:'Iphone',
+        correct:'a'
+    }
+
+    ]
